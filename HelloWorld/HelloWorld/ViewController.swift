bad29cb1305e42e43bc46377659c2e3f72f22b69
Admin <admin@Admins-Mac.local>
2017-02-03 18:24:57 +0530

    Test Project Hello World

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Admin on 14/11/1938 Saka.
+//  Copyright © 1938 Saka AshwaMegh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
